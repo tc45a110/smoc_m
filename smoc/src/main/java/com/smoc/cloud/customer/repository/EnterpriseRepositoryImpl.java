cc9095247ef832ef0178d2907e35dd5defae4e43
chen <xiaoqiang625086245@qq.com>
2022-02-10 15:19:22 +0800

    持续更新

--- smoc/src/main/java/com/smoc/cloud/customer/repository/EnterpriseRepositoryImpl.java
+++ smoc/src/main/java/com/smoc/cloud/customer/repository/EnterpriseRepositoryImpl.java
@@ -36,13 +36,22 @@ public class EnterpriseRepositoryImpl extends BasePageRepository {
         sqlBuffer.append(", t.ENTERPRISE_STATUS");
         sqlBuffer.append(", DATE_FORMAT(t.CREATED_TIME, '%Y-%m-%d %H:%i:%S')CREATED_TIME");
         sqlBuffer.append("  from enterprise_basic_info t ");
-        sqlBuffer.append("  where t.ENTERPRISE_PARENT_ID='0000' ");
+        sqlBuffer.append("  where 1=1 ");
 
         List<Object> paramsList = new ArrayList<Object>();
 
         if (!StringUtils.isEmpty(qo.getEnterpriseId())) {
             sqlBuffer.append(" and t.ENTERPRISE_ID = ? ");
             paramsList.add(qo.getEnterpriseId().trim());
+        }
+
+        if(!StringUtils.isEmpty(qo.getEnterpriseParentId())){
+            sqlBuffer.append(" and t.ENTERPRISE_PARENT_ID = ? ");
+            paramsList.add(qo.getEnterpriseParentId().trim());
+        }else{
+            sqlBuffer.append(" and t.ENTERPRISE_PARENT_ID = ? ");
+            qo.setEnterpriseParentId("0000");
+            paramsList.add("0000");
         }
 
         if (!StringUtils.isEmpty(qo.getEnterpriseName())) {
@@ -90,11 +99,13 @@ public class EnterpriseRepositoryImpl extends BasePageRepository {
         pageList.getPageParams().setParams(qo);
 
         //查询二级企业信息
-        List<EnterpriseBasicInfoValidator> list = pageList.getList();
-        for (int i = 0; i < list.size(); i++) {
-            EnterpriseBasicInfoValidator basicInfo = list.get(i);
-            List<EnterpriseBasicInfoValidator> validator = findByEnterpriseParentId(basicInfo);
-            basicInfo.setEnterprises(validator);
+        if("0000".equals(qo.getEnterpriseParentId())){
+            List<EnterpriseBasicInfoValidator> list = pageList.getList();
+            for (int i = 0; i < list.size(); i++) {
+                EnterpriseBasicInfoValidator basicInfo = list.get(i);
+                List<EnterpriseBasicInfoValidator> validator = findByEnterpriseParentId(basicInfo);
+                basicInfo.setEnterprises(validator);
+            }
         }
 
         return pageList;
