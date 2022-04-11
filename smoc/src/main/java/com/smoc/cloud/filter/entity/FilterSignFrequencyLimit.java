4c94a5ad1a6900471d357bad47fcfcc9fb1ae881
chen <xiaoqiang625086245@qq.com>
2022-04-11 15:49:19 +0800

    持续更新

---
+++ smoc/src/main/java/com/smoc/cloud/filter/entity/FilterSignFrequencyLimit.java
@@ -0,0 +1,143 @@
+package com.smoc.cloud.filter.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "filter_sign_frequency_limit")
+public class FilterSignFrequencyLimit {
+    private String id;
+    private String limitType;
+    private String infoType;
+    private String signs;
+    private String accounts;
+    private String frequency;
+    private String createdBy;
+    private Date createdTime;
+    private String updatedBy;
+    private Date updatedTime;
+
+    @Id
+    @Column(name = "ID")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "LIMIT_TYPE")
+    public String getLimitType() {
+        return limitType;
+    }
+
+    public void setLimitType(String limitType) {
+        this.limitType = limitType;
+    }
+
+    @Basic
+    @Column(name = "INFO_TYPE")
+    public String getInfoType() {
+        return infoType;
+    }
+
+    public void setInfoType(String infoType) {
+        this.infoType = infoType;
+    }
+
+    @Basic
+    @Column(name = "SIGNS")
+    public String getSigns() {
+        return signs;
+    }
+
+    public void setSigns(String signs) {
+        this.signs = signs;
+    }
+
+    @Basic
+    @Column(name = "ACCOUNTS")
+    public String getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(String accounts) {
+        this.accounts = accounts;
+    }
+
+    @Basic
+    @Column(name = "FREQUENCY")
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    @Basic
+    @Column(name = "CREATED_BY")
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Basic
+    @Column(name = "CREATED_TIME")
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    @Basic
+    @Column(name = "UPDATED_BY")
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Basic
+    @Column(name = "UPDATED_TIME")
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FilterSignFrequencyLimit that = (FilterSignFrequencyLimit) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(limitType, that.limitType) &&
+                Objects.equals(infoType, that.infoType) &&
+                Objects.equals(signs, that.signs) &&
+                Objects.equals(accounts, that.accounts) &&
+                Objects.equals(frequency, that.frequency) &&
+                Objects.equals(createdBy, that.createdBy) &&
+                Objects.equals(createdTime, that.createdTime) &&
+                Objects.equals(updatedBy, that.updatedBy) &&
+                Objects.equals(updatedTime, that.updatedTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, limitType, infoType, signs, accounts, frequency, createdBy, createdTime, updatedBy, updatedTime);
+    }
+}
