6878da380b6501c30e8964da06b9c139db146b03
武 <wujihui@manpuman.com>
2022-06-16 18:12:42 +0800

    项目进行中

---
+++ smoc-gateway/src/main/java/com/somc/cloud/gateway/configuration/ReactNettyConfiguration.java
@@ -0,0 +1,24 @@
+package com.somc.cloud.gateway.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+
+/**
+ * Netty 线程调优
+ */
+@Slf4j
+@Configuration
+public class ReactNettyConfiguration {
+
+    @Bean
+    public ReactorResourceFactory reactorClientResourceFactory() {
+        System.setProperty("reactor.netty.ioSelectCount", "1");
+        System.setProperty("reactor.ipc.netty.selectCount", "1");
+        // 这里工作线程数为2-4倍都可以。看具体情况
+        System.setProperty("reactor.netty.ioWorkerCount", String.valueOf(Math.max(Runtime.getRuntime().availableProcessors() * 4, 4)));
+        System.setProperty("reactor.ipc.netty.workerCount", String.valueOf(Math.max(Runtime.getRuntime().availableProcessors() * 4, 4)));
+        return new ReactorResourceFactory();
+    }
+}
