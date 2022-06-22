463a01d63ccde2363f10fd2a1b865a195534dfa4
siyan <2829214322@qq.com>
2022-06-22 16:17:17 +0800

    持续更新

---
+++ smoc-protocol-proxy-smgp/src/main/java/com/protocol/proxy/worker/ReportWorker.java
@@ -0,0 +1,179 @@
+ /**
+ * @desc
+ * 从通道表中按照优先级及时间先后获取数据，每次按照通道的速率进行获取，存入到队列中
+ */
+package com.protocol.proxy.worker;
+
+import com.alibaba.druid.util.Base64;
+import com.base.common.cache.CacheBaseService;
+import com.base.common.constant.DynamicConstant;
+import com.base.common.constant.FixedConstant;
+import com.base.common.constant.InsideStatusCodeConstant;
+import com.base.common.log.CategoryLog;
+import com.base.common.manager.ChannelInfoManager;
+import com.base.common.manager.ChannelMOManager;
+import com.base.common.manager.LongSMSMOManager;
+import com.base.common.util.ChannelMOUtil;
+import com.base.common.util.DateUtil;
+import com.base.common.vo.BusinessRouteValue;
+import com.base.common.vo.ChannelMO;
+import com.base.common.worker.SuperQueueWorker;
+import com.huawei.insa2.comm.smgp.Common;
+import com.huawei.insa2.comm.smgp.message.SMGPDeliverMessage;
+import com.huawei.insa2.comm.smgp.message.SMGPMessage;
+import com.huawei.insa2.util.TypeConvert;
+
+public class ReportWorker extends SuperQueueWorker<SMGPMessage>{
+	private String channelID;
+	
+	public ReportWorker(String channelID,String index) {
+		this.channelID = channelID;
+		init();
+		//先判断表是否存在，初始化时会建表
+		this.setName(new StringBuilder(channelID).append("-").append(index).toString());
+		this.start();
+	}
+	
+	public void process(SMGPMessage message){
+		add(message);
+	}
+	
+	/**
+	 * 初始化:检查通道表
+	 */
+	private void init(){
+		
+	}
+
+
+	@Override
+	protected void doRun() throws Exception {
+		SMGPMessage message = take();
+		CategoryLog.messageLogger.info(message.toString());
+		int registeredDeliver = 0;
+		//短信是否包含长短信头的标识
+		int tpUdhi=0;
+		int messageFormat=0;
+		String channelReportSRCID=null;
+		String phoneNumber=null;
+		String subStatusCode = null;
+		byte[] messageContent=null;
+		byte[] channelMessageID=null;
+		String statusCode=null;
+		String sequenceID=null;
+		
+		sequenceID = String.valueOf(message.getSequenceId());
+		
+		SMGPDeliverMessage deliverMessage = (SMGPDeliverMessage)message;
+		registeredDeliver = deliverMessage.getIsReport();
+		messageFormat = deliverMessage.getMsgFormat();
+		phoneNumber = deliverMessage.getSrcTermID();
+		channelReportSRCID = deliverMessage.getDestTermID();
+		channelMessageID = deliverMessage.getMsgId();
+		messageContent = deliverMessage.getMsgContent();
+		statusCode = deliverMessage.getStat();
+		subStatusCode = deliverMessage.getErr();
+		
+		//状态报告
+		if(registeredDeliver == 1){
+			processChannelReport(statusCode, subStatusCode, channelReportSRCID, channelMessageID, phoneNumber,sequenceID);
+		}else if(registeredDeliver == 0){
+			processChannelMO(tpUdhi, messageFormat, phoneNumber, channelReportSRCID, messageContent,sequenceID);
+		}
+		
+	}
+	
+	/**
+	 * 处理状态报告
+	 * @param statusCode
+	 * @param channelReportSRCID
+	 * @param channelMessageID
+	 * @param phoneNumber
+	 */
+	private void processChannelReport(String statusCode, String subStatusCode,String channelReportSRCID,byte[] channelMessageID,String phoneNumber,String sequenceID){
+		logger.info(
+				new StringBuilder().append("回执信息")
+				.append("{}channelID={}")
+				.append("{}phoneNumber={}")
+				.append("{}channelMessageID={}")
+				.append("{}channelReportSRCID={}")
+				.append("{}statusCode={}")
+				.append("{}subStatusCode={}")
+				.append("{}sequenceID={}")
+				.toString(),
+				FixedConstant.SPLICER,channelID,
+				FixedConstant.SPLICER,phoneNumber,
+				FixedConstant.SPLICER,String.valueOf(TypeConvert.byte2long(channelMessageID)),
+				FixedConstant.SPLICER,channelReportSRCID,
+				FixedConstant.SPLICER,statusCode,
+				FixedConstant.SPLICER,subStatusCode,
+				FixedConstant.SPLICER,sequenceID
+				);
+		
+		BusinessRouteValue businessRouteValue = new BusinessRouteValue();
+		
+		businessRouteValue.setChannelID(channelID);
+		businessRouteValue.setStatusCode(statusCode);
+		businessRouteValue.setSubStatusCode(subStatusCode);
+		businessRouteValue.setStatusCodeSource(FixedConstant.StatusReportSource.CHANNEL.name());
+		//根据协议层设置或默认的成功状态码判断后设置成功码标识
+		if(DynamicConstant.REPORT_SUCCESS_CODE.equals(statusCode)){
+			businessRouteValue.setSuccessCode(InsideStatusCodeConstant.SUCCESS_CODE);
+		}else{
+			businessRouteValue.setSuccessCode(InsideStatusCodeConstant.FAIL_CODE);
+		}
+		businessRouteValue.setChannelReportSRCID(channelReportSRCID);
+		businessRouteValue.setChannelMessageID(Common.bytes2hex(channelMessageID));
+		businessRouteValue.setPhoneNumber(phoneNumber);
+		businessRouteValue.setChannelReportTime(DateUtil.getCurDateTime(DateUtil.DATE_FORMAT_COMPACT_STANDARD_MILLI));
+		
+		CacheBaseService.saveReportToMiddlewareCache(businessRouteValue);
+	}
+	
+	/**
+	 * 处理上行信息
+	 * @param tpUdhi
+	 * @param messageFormat
+	 * @param phoneNumber
+	 * @param channelMOSRCID
+	 * @param messageContent
+	 */
+	private void processChannelMO(int tpUdhi,int messageFormat,String phoneNumber,String channelMOSRCID,byte[] messageContent,String sequenceID){
+		logger.info("上行信息{}{}{}{}{}{}{}{}{}{}{}{}{}{}",
+				new StringBuilder().append("上行信息")
+				.append("{}channelID={}")
+				.append("{}phoneNumber={}")
+				.append("{}channelMOSRCID={}")
+				.append("{}messageContentBase64={}")
+				.append("{}tpUdhi={}")
+				.append("{}messageFormat={}")
+				.append("{}sequenceID={}")
+				.toString(),
+				FixedConstant.SPLICER,channelID,
+				FixedConstant.SPLICER,phoneNumber,
+				FixedConstant.SPLICER,channelMOSRCID,
+				FixedConstant.SPLICER,Base64.byteArrayToBase64(messageContent),
+				FixedConstant.SPLICER,tpUdhi,
+				FixedConstant.SPLICER,messageFormat,
+				FixedConstant.SPLICER,sequenceID
+				);
+		
+		String channelSRCID = ChannelInfoManager.getInstance().getChannelSRCID(channelID);
+		ChannelMO channelMO = ChannelMOUtil.getMO(tpUdhi, messageFormat, phoneNumber, channelMOSRCID, messageContent,channelID,channelSRCID);
+		//长短信需要进行合成处理
+		if(channelMO.getMessageTotal() > 1){
+			LongSMSMOManager.getInstance().add(channelMO);
+		}else {
+			ChannelMOManager.getInstance().add(channelMO);
+		}
+	}
+	
+	
+	public void exit(){
+		//停止线程
+		super.exit();
+	}
+	
+}
+
+
---
+++ smoc-protocol-proxy-smgp/src/main/java/com/protocol/proxy/worker/ResponseWorker.java
@@ -0,0 +1,176 @@
+/**
+ * @desc
+ * 
+ */
+package com.protocol.proxy.worker;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+
+import com.base.common.cache.CacheBaseService;
+import com.base.common.constant.DynamicConstant;
+import com.base.common.constant.FixedConstant;
+import com.base.common.constant.InsideStatusCodeConstant;
+import com.base.common.manager.ChannelInfoManager;
+import com.base.common.vo.BusinessRouteValue;
+import com.base.common.worker.SuperMapWorker;
+import com.huawei.insa2.comm.smgp.Common;
+import com.huawei.insa2.comm.smgp.message.SMGPMessage;
+import com.huawei.insa2.comm.smgp.message.SMGPSubmitRespMessage;
+
+public class ResponseWorker extends SuperMapWorker<Integer, TimerTask>{
+	
+	private String channelID;
+	private Timer timer;
+	
+	private BlockingQueue<SMGPMessage> responseQueue;
+	
+	
+
+	ResponseWorker(String channelID,String index,BlockingQueue<SMGPMessage> responseQueue) {
+		this.responseQueue = responseQueue;
+		timer = new Timer();
+		this.setName(new StringBuilder(channelID).append("-").append(index).toString());
+		this.start();
+	}
+	
+	/**
+	 * 设置响应超时任务
+	 * @param sequenceID
+	 * @param businessRouteValue
+	 */
+	void setResponseTimeoutTask(int sequenceID,BusinessRouteValue businessRouteValue){
+		TimerTask responseTimeoutTask = new ResponseTimeoutTask(sequenceID,businessRouteValue);
+		add(sequenceID, responseTimeoutTask);
+		//长链接：获取提交等待响应的超时时间，一般是通用时间，可以根据单个通道在通道扩展参数中设置responseTimeout
+		long delay = ChannelInfoManager.getInstance().getResponseTimeout(channelID);
+		timer.schedule(responseTimeoutTask, delay);
+	}
+	
+	/**
+	 * 判断未知响应是否超过滑动窗口大小
+	 * @return
+	 */
+	boolean overGlideWindow(){
+		return size() >= ChannelInfoManager.getInstance().getGlideWindowSize(channelID);
+	}
+
+	@Override
+	protected void doRun() throws Exception {
+		SMGPMessage message = responseQueue.take();
+		int sequenceID = message.getSequenceId();
+		ResponseTimeoutTask responseTimeoutTask = (ResponseTimeoutTask)remove(sequenceID);
+		
+		if(responseTimeoutTask == null){
+	
+			logger.info(new StringBuilder().append("响应信息")
+					.append("{}sequenceID={}")
+					.toString(),
+					FixedConstant.SPLICER,sequenceID
+					);
+			return;
+		}
+		responseTimeoutTask.cancel();
+		BusinessRouteValue businessRouteValue = responseTimeoutTask.getBusinessRouteValue();
+
+		byte[] channelMessageID = null;
+		String nextNodeErrorCode = "";
+		
+		SMGPSubmitRespMessage responseMessage = (SMGPSubmitRespMessage) message;
+		channelMessageID = responseMessage.getMsgId();
+		nextNodeErrorCode = String.valueOf(responseMessage.getStatus());
+		
+		//记录msgid、提交成功标识、提交响应码
+		businessRouteValue.setChannelMessageID(Common.bytes2hex(channelMessageID));
+		businessRouteValue.setNextNodeErrorCode(nextNodeErrorCode);
+		
+		if(DynamicConstant.RESPONSE_SUCCESS_CODE.equals(nextNodeErrorCode)){
+			businessRouteValue.setNextNodeCode(InsideStatusCodeConstant.SUCCESS_CODE);
+		}else{
+			businessRouteValue.setNextNodeCode(InsideStatusCodeConstant.FAIL_CODE);
+		}
+		logger.info(
+				new StringBuilder().append("响应信息")
+				.append("{}accountID={}")
+				.append("{}phoneNumber={}")
+				.append("{}messageContent={}")
+				.append("{}channelID={}")
+				.append("{}channelTotal={}")
+				.append("{}channelIndex={}")
+				.append("{}sequenceID={}")
+				.append("{}channelMessageID={}")
+				.append("{}responseCode={}")
+				.toString(),
+				FixedConstant.SPLICER,businessRouteValue.getAccountID(),
+				FixedConstant.SPLICER,businessRouteValue.getPhoneNumber(),
+				FixedConstant.SPLICER,businessRouteValue.getMessageContent(),
+				FixedConstant.SPLICER,businessRouteValue.getChannelID(),
+				FixedConstant.SPLICER,businessRouteValue.getChannelTotal(),
+				FixedConstant.SPLICER,businessRouteValue.getChannelIndex(),
+				FixedConstant.SPLICER,sequenceID,
+				FixedConstant.SPLICER,businessRouteValue.getChannelMessageID(),
+				FixedConstant.SPLICER,businessRouteValue.getNextNodeErrorCode()
+				);
+		CacheBaseService.saveResponseToMiddlewareCache(businessRouteValue);
+	}
+	
+	public void exit(){
+		super.exit();
+	}
+	
+	/**
+	 * 在设置的超时时间内没有
+	 */
+	class ResponseTimeoutTask extends TimerTask {
+
+		private int sequenceID;
+		private BusinessRouteValue businessRouteValue;
+
+		public ResponseTimeoutTask(int sequenceID,BusinessRouteValue businessRouteValue) {
+			super();
+			this.sequenceID = sequenceID;
+			this.businessRouteValue = businessRouteValue;
+		}
+
+		@Override
+		public void run() {
+			try {
+				remove(sequenceID);
+				logger.info(
+						new StringBuilder().append("提交无响应")
+						.append("{}accountID={}")
+						.append("{}phoneNumber={}")
+						.append("{}messageContent={}")
+						.append("{}channelID={}")
+						.append("{}channelTotal={}")
+						.append("{}channelIndex={}")
+						.append("{}sequenceID={}")
+						.toString(),
+						FixedConstant.SPLICER,businessRouteValue.getAccountID(),
+						FixedConstant.SPLICER,businessRouteValue.getPhoneNumber(),
+						FixedConstant.SPLICER,businessRouteValue.getMessageContent(),
+						FixedConstant.SPLICER,businessRouteValue.getChannelID(),
+						FixedConstant.SPLICER,businessRouteValue.getChannelTotal(),
+						FixedConstant.SPLICER,businessRouteValue.getChannelIndex(),
+						FixedConstant.SPLICER,sequenceID
+						);
+				//未知
+				businessRouteValue.setNextNodeCode(InsideStatusCodeConstant.UNKNOWN_CODE);
+				businessRouteValue.setNextNodeErrorCode("");
+				businessRouteValue.setChannelMessageID("");
+				CacheBaseService.saveResponseToMiddlewareCache(businessRouteValue);
+			} catch (Exception e) {
+				logger.error(e.getMessage(),e);
+			}
+		}
+
+		public BusinessRouteValue getBusinessRouteValue() {
+			return businessRouteValue;
+		}
+		
+	}
+	
+}
+
+
