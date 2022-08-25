71516b0ed4ba782486ac1367ba1d4e1cbd459ae6
siyan <2829214322@qq.com>
2022-08-25 16:02:07 +0800

    更新中

--- smoc-backend-business-statistics/src/main/resources/xingyu/scripts/account.alarm.message.sh
+++ smoc-backend-business-statistics/src/main/resources/xingyu/scripts/account.alarm.message.sh
@@ -4,4 +4,4 @@
 
 #监控账号的成功率，延迟率`
 cd /smoc/apps/business-statistics
-java -classpath .:./config:./lib/* com.business.statistics.message.alarm.AccountAlarm -10 >> /alidata/business-statistics/logs/`date "+%Y-%m-%d"`.log &
+java -classpath .:./config:./lib/* com.business.statistics.message.alarm.AccountAlarm -10 >> /smoc/logs/business-statistics/`date "+%Y-%m-%d"`.log &
