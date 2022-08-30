d4053d336c569d5d447e85c5de04571d6d1582c1
武 <wujihui@manpuman.com>
2022-08-30 15:06:30 +0800

    项目更新

--- smoc-iot/src/main/java/com/smoc/cloud/iot/carrier/repository/IotFlowCardsSecondaryInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smoc.cloud.iot.carrier.repository;
-
-import com.smoc.cloud.iot.carrier.entity.IotFlowCardsSecondaryInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IotFlowCardsSecondaryInfoRepository extends JpaRepository<IotFlowCardsSecondaryInfo, String> {
-}
